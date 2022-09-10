521d5618a693572ee8f1113a67ad84695cdc74e9
Rajeev <s2100155@edu.bc.fi>
2022-09-10 08:15:00 +0300

    exercise squared, squared root and absolute value completed

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,13 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Please write number ");
+        int number1 = Integer.parseInt(scanner.nextLine());
+        if ( number1 < 0) {
+            int  result = number1 * -1;
+            System.out.println(result);
+        } else {
+            System.out.println(number1);
+        }
     }
 }
